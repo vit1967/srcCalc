c868752611200a4f9b9e5122464d1ed9e308a386
vit1967@inbox.ru <vit1967@inbox.ru>
2019-09-24 18:02:04 +0300

    0

--- srcCalc/src/Main.java
+++ srcCalc/src/Main.java
@@ -22,7 +22,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        System.out.println("Введи выр. вида 121 +43- 64 +765*5 / 2 или римск. вида VII+IIL - XVI с любым кол-вом оп.");
+        System.out.println("Введи выр. вида 1+9 или 121 +43- 64 +765*5 / 2 или римск. вида VII+IIL - XVI .");
 //        String input = "878979+287372 -787 + 7564 / IVXL * 576 + LXII ";
 
         Scanner sc=new Scanner(System.in);
