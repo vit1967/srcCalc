62fa512cbb4c7d058bd49f82071ff7505a9fdb4a
vit1967@inbox.ru <vit1967@inbox.ru>
2019-10-02 00:21:23 +0300

    0

--- srcCalc/src/test/main/StringParserTest.java
+++ srcCalc/src/test/main/StringParserTest.java
@@ -1,15 +1,37 @@
 package main;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class StringParserTest {
 
     @Test
-    public void inputToInt() {
+    public void inputToIntFromArab() {
+        Main.tipRA= Main.RomArab.Arab;
+        try {
+            Assert.assertEquals("Не считывается 5 в тесте",5,StringParser.inputToInt("5"));
+        } catch (NoValidateTipExeption noValidateTipExeption) {
+            noValidateTipExeption.printStackTrace();
+        }
     }
-
+    @Test
+    public void inputToIntFromRom() {
+        Main.tipRA= Main.RomArab.Rom;
+        try {
+            Assert.assertEquals("Не считывается VII ->7 в тесте",7,StringParser.inputToInt("VII"));
+        } catch (NoValidateTipExeption noValidateTipExeption) {
+            noValidateTipExeption.printStackTrace();
+        }
+    }
     @Test
     public void rezToString() {
+        Main.tipRA= Main.RomArab.Rom;
+
+        try {
+            Assert.assertEquals("Не выводится VII <-7 в тесте","VII",StringParser.rezToString(7));
+        } catch (NoValidateTipExeption noValidateTipExeption) {
+            noValidateTipExeption.printStackTrace();
+        }
     }
 
     @Test
